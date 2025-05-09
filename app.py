e9a0a7901de9ff4cef503d02dcff3851eef484a5
abhimanue sinha <abhimanue1291@gmail.com>
2025-05-09 20:17:23 +0530

    Added the Flask project

--- app.py
+++ app.py
@@ -2,6 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India",
+        "Salary": "Rs. 10,00,000"
+
+    },
+    {
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Delhi, India",
+        "Salary": "Rs. 15,00,000"
+
+    },
+    {
+        "id": 3,
+        "title": "Frontend Engineer",
+        "location": "Remote",
+        "Salary": "Rs. 12,00,000"
+
+    },
+    {
+        "id": 4,
+        "title": "Backend Engineer",
+        "location": "San Francisco, USA",
+        "location": "Remote",
+        "Salary": "$120,000"
+
+    }
+
+
+    ]
+
 @app.route("/")
 def hello_world():
     return render_template("home.html")
